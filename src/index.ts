84c8ff11c2618bb6e11b19ed91dbae4be3b6df14
Shirish Maharjan <sirimhrzn@gmail.com>
2023-09-14 21:50:38 +0545

    poller works as intended, testing needed
    can read from sources.json and write to state.json for holding state of etag many more

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,19 @@ import { format } from 'date-fns';
 import * as cheerio from 'cheerio';
 import { time } from "console";
 import { getImage } from "./getImage"
+import et from "date-fns/locale/et";
+import { exit } from "process";
 const md5 = require('md5');
 const { v4: uuidv4 } = require('uuid');
 
 const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
 
 
-const sourceList: SourceDetails = {
+const sourceList: SourceDetails = await Bun.file("src/sources.json").json()
+const souceList: SourceDetails = {
   "FRDH": {
     "feed_urls": {
-      "tops": "https://arthasarokar.com/feeds/posts/default/-/NEPALI%20PATRO?alt=rss",
-      "aisa": "https://wwe/bisasd.com"
+      "oths": "http://arthadabali.com/feed",
     },
     "tier_config": {
       "interval_value": "900",
@@ -24,82 +26,97 @@ const sourceList: SourceDetails = {
   }
 };
 
-const source = "FRDH";
+//const source = "FRDH";
 const timeFormat = "yyyy-MM-dd H:m:s";
-const feed_urls = sourceList[source].feed_urls;
+//const feed_urls = sourceList[source].feed_urls;
 const currentTime = format(new Date(), timeFormat);
 const STATEJSON = "state.json";
 
-var feedBuildCount: number = 0;
 let stateJSON = Bun.file(STATEJSON);
-let state: State | undefined = undefined;
-if (stateJSON) {
-  state = await Bun.file(STATEJSON).json().then((data) => {
-    return data
-  }).catch(() => {
-    console.log(`[${currentTime}] ${STATEJSON} file doesnot exists. `)
-  });
-}
-console.log(state)
-for (const categories in feed_urls) {
-  if (!categoryArray.includes(categories)) {
-    console.log(`[${currentTime}] ${categories} category doesnot exist `)
-    continue;
-  }
-  const category: Category = categories as Category;
 
-  const url = feed_urls[category] ?? '';
-  if (url == '') {
-    console.log(`[${currentTime}] ${source}-${category} URL EMPTY`)
-    continue;
-  }
-  const md5Url = md5(url);
-  console.log(`[${currentTime}] RSS processing ${url}`);
-  let fetchFail = false;
-  await fetch(url)
-    .then(async (rssData) => {
-      let data = await rssData.text();
-      let previousEtag: string | undefined;
-      if (state !== undefined) {
-        previousEtag = state[source][category];
-      }
-      let etag = rssData.headers.get("etag")?.replace(/"/g, '') || '';
-      if ((etag !== '' && previousEtag) && previousEtag == etag) {
-        console.log(`[${currentTime}] Etag for ${source}-${category} not changed`)
-        fetchFail = true;
-        return
-      }
-      console.log(etag + `-${source}-${category}`);
-      const options = {
-        ignoreAttributes: false,
-        attributeNamePrefix: ""
-      };
-      let parser = new XMLParser(options);
-      let feed: RSSDetail = parser.parse(data);
-      feed.rss.channel.item.forEach(async (news) => {
-        if (news.title == undefined) {
-          fetchFail = true
-          console.log(`[${currentTime}] Skipping ${news.link}: TITLE EMPTY`)
+//var feedBuildCount: number = 0;
+
+for (const source in sourceList) {
+  for (const categories in sourceList[source].feed_urls) {
+    let file = await stateJSON.exists();
+    let dataExists = false;
+    var state: State = {};
+    if (file) {
+      await Bun.file(STATEJSON).json().then((data) => {
+        state = data;
+        dataExists = true;
+      }).catch((err) => {
+        console.log(`[${currentTime}] ${STATEJSON} file doesnot exists. ${err}`)
+      });
+    }
+    if (!categoryArray.includes(categories)) {
+      console.log(`[${currentTime}] ${categories} category doesnot exist `)
+      continue;
+    }
+    const category: Category = categories as Category;
+    const url = sourceList[source].feed_urls[category] ?? '';
+    if (url == '') {
+      console.log(`[${currentTime}] ${source}-${category} URL EMPTY`)
+      continue;
+    }
+    const md5Url = md5(url);
+    console.log(`[${currentTime}] RSS processing ${url}`);
+    let fetchFail = false;
+    let etag: string = '';
+    await fetch(url)
+      .then(async (rssData) => {
+        let data = await rssData.text();
+        let previousEtag: string | undefined;
+        if (dataExists && state[source]) {
+          previousEtag = state[source][category];
+        }
+        etag = rssData.headers.get("etag")?.replace(/"/g, '') ?? '';
+        if ((etag !== '' && previousEtag) && previousEtag == etag) {
+          console.log(`[${currentTime}] Etag for ${source}-${category} not changed`)
+          fetchFail = true;
           return
         }
-        let feedBuilder = await FeedBuilder(category, news);
-        //console.log(feedBuilder);
-        feedBuildCount += 1;
-        if (etag !== '' && state !== undefined) {
-          state[source][category] = etag;
-          await Bun.write(STATEJSON, state);
-        }
-      })
-    }).catch((err) => {
-      console.log(`[${currentTime}] Error processing ${url} \n ${err}`)
-      fetchFail = true;
-    });
-  if (fetchFail) {
-    console.log(`[${currentTime}] Skipping RSS ${url}`);
-    continue;
+        const options = {
+          ignoreAttributes: false,
+          attributeNamePrefix: ""
+        };
+        let parser = new XMLParser(options);
+        let feed: RSSDetail = parser.parse(data);
+        feed.rss.channel.item.forEach(async (news) => {
+          if (news.title == undefined) {
+            fetchFail = true
+            console.log(`[${currentTime}] Skipping ${news.link}: TITLE EMPTY`)
+            return
+          }
+        })
+      }).catch((err) => {
+        console.log(`[${currentTime}] Error processing ${url} ${err}`)
+        fetchFail = true;
+      });
+    if (fetchFail) {
+      console.log(`[${currentTime}] Skipping RSS ${url}`);
+      fetchFail = false
+      continue;
+    }
+    if (etag !== '' || dataExists) {
+      if (!state[source] && etag == '') {
+        console.log(`[${currentTime}] no etag for ${source}-${category}`)
+        continue
+      }
+      if (!state[source]) {
+        state[source] = {
+          [category]: etag
+        };
+      } else {
+        state[source][category] = etag
+      }
+      await Bun.write(STATEJSON, JSON.stringify(state));
+      console.log(`[${currentTime}] Adding ${source} with etag to ${STATEJSON}`)
+    }
+
   }
 }
-
+console.log(state)
 async function FeedBuilder(category: string, news: NewsItem): Promise<News> {
   let strippedContent = news["content:encoded"] ? cheerio.load(news["content:encoded"]).text() ?? "" : "";
   let strippedDescription = news.description ? cheerio.load(news.description).text() ?? "" : "";
@@ -123,7 +140,6 @@ async function FeedBuilder(category: string, news: NewsItem): Promise<News> {
 const wordChecker = (title: string, word: string): boolean => {
   return title.includes(word)
 }
-console.log(feedBuildCount)
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
